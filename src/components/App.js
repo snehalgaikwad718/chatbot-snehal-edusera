7dd85aabd34c4a1837032bb1732d3f087085ef3c
Snehal Gaikwad <snehalgaikwad0201@gmail.com>
2021-08-01 16:11:47 +0530

    Styles for ChatBot trigger added.

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,15 @@
 import React, { Component } from 'react'
 import Header from './Bootstrap/Header'
+import ChatBot from './ChatBot/ChatBot'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Header containerclass="justify-content-center" dark={true}>ChatBot Without AI</Header>
+        <Header containerclass="justify-content-center" dark={true}>
+          ChatBot Without AI
+        </Header>
+        <ChatBot/>
       </div>
     )
   }
