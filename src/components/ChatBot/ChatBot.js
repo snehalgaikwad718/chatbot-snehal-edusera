d184a3b5667220f446f1496caa9b4dba9694260a
Snehal Gaikwad <snehalgaikwad0201@gmail.com>
2021-08-01 17:05:22 +0530

    Added some dummy messages with styles.

--- src/components/ChatBot/ChatBot.js
+++ src/components/ChatBot/ChatBot.js
@@ -1,14 +1,33 @@
 import React, { Component } from 'react'
 import ChatBotImg from "../../assests/chatbot.png"
-
+import GuestImg from "../../assests/guest.png"
 class ChatBot extends Component {
     render() {
         return (
             <div className="ChatBot-Wrapper">
                 <div className="ChatBot-Message">
-                    <div className="ChatBot-Trigger">
-                        <img src={ChatBotImg} alt="Open Chat" />
-                    </div>
+                    <ul>
+                        {[{
+                            Text: "Hello, I'm a ChatBot!",
+                            Bot: true
+                        },
+                        {
+                            Text: "Hello, ChatBot",
+                            Bot: false
+                        }].map((msg, key) => (
+                            <li key={key} className={msg.Bot ? "ChatBotImg" : "GuestImg"}>
+                                <img 
+                                src={msg.Bot ? ChatBotImg : GuestImg} 
+                                alt={msg.Bot ? "ChatBotImg" : "GuestImg"} 
+                                />
+                                <p>{msg.Text}</p>
+                            </li>
+                        ))}
+                    </ul>
+                </div>
+                <form className="ChatBot-Input"></form>
+                <div className="ChatBot-Trigger">
+                    <img src={ChatBotImg} alt="Open Chat" />
                 </div>
             </div>
         )
